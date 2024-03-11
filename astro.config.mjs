7bdc3867dc7457857574ffda0721c640b9f8ec95
Alvaro Vanegas <alvaro@ajvanegasv.dev>
2024-03-11 18:24:23 -0500

    restart project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-
-import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), vue(), icon()]
-});
+export default defineConfig({});
