3c1bba2d95c995200d2b2665852d58ac7b576875
Alvaro Vanegas <alvaro@ajvanegasv.dev>
2024-03-12 22:26:54 -0500

    add first section in index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,10 +7,17 @@ export default {
 				'ted': ['TAN-NIMBUS']
 			},
 			colors: {
-				'ted-green': '#142d17'
+				'ted-green': '#142d17',
+				'ted-gray': '#879788'
 			},
 			backgroundImage: {
 				'ted-image': "url('/ted-image-1.png')"
+			},
+			fontSize: {
+				'xl-ted': ['1.25rem', '1.5625rem']
+			},
+			height: {
+				'85': '21.1875rem'
 			}
 		},
 	},
